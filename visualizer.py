567c80ee3277f4e476649b1f88b51f6bb88ca8ef
Marek Onuszko <marek.onuszko@gmail.com>
2017-09-22 15:42:15 +0200

    OO-based refactoring

--- visualizer.py
+++ visualizer.py
@@ -28,7 +28,6 @@ VARIANTS = {
         'Consume-x2': 'Consume',
         }
 
-
 ROMAN = {
     'Explore': 'I',
     'Develop': 'II',
@@ -36,12 +35,6 @@ ROMAN = {
     'Consume': 'IV',
     'Produce': 'V'
 }
-
-
-def get_color(player):
-    colors = ('red', 'green', 'yellow', 'cyan')
-    color = player.lower() if player.lower() in colors else 'blue'
-    return color
 
 
 logs = [f for f in os.listdir('/tmp') if f.startswith('export_')]
@@ -66,14 +59,101 @@ def get_phase_name(choice):
     return choice if choice not in VARIANTS else VARIANTS[choice]
 
 
-def updated_choices(choices, msg):
-    # TODO: 2 Player Advanced needs 2 choices per player.
-    if ' chooses ' in msg:
-        player_name, choice = re.search(r'(.+) chooses (.+)\.', msg).groups()
-        # Split to support 2 Player Advanced:
-        for ch in choice.split('/'):
-            choices.append((player_name, ch))
-    return choices
+class Round():
+    def __init__(self, number):
+        self.phases = []
+        self.number = number
+        self.choices = []
+
+    def update_choices(self, msg):
+
+        def phase_order(choice):
+            return PHASES.index(get_phase_name(choice[1]))
+
+        if ' chooses ' in msg:
+            player_name, choice = re.search(r'(.+) chooses (.+)\.', msg).groups()
+            # Split to support 2 Player Advanced:
+            for ch in choice.split('/'):
+                self.choices.append((player_name, ch))
+        choices = sorted(self.choices, key=phase_order)
+
+    def render_header(self):
+        line('td', 'Actions')
+        #TODO: Do Players really belong inside Phase ? Think of pros and cons.
+        for player in self.phases[0].players:
+            tab = '#' * len(player.tableau)
+            # Split tableau into groups of 4 because humans can't naturally
+            # perceive amounts higher than 4.
+            choices = (ch[1] for ch in self.choices if ch[0] == player.name)
+            choices = '/'.join(choices)
+            tab = [tab[n:n+4] for n in range(0, len(tab), 4)]
+            tab = ' '.join(tab)
+            with tag('td', klass=player.get_color()):
+                with tag('ul'):
+                    line('li', '{0} ({1})'.format(player.name, choices))
+                    line('li', tab)
+
+    # TODO: maybe a phase method?
+    # Feature envy ?
+    def phase_played_by(self, phase):
+        '''Return list of player names who made this phase possible.'''
+        names = []
+        for ch in self.choices:
+            if phase.name == get_phase_name(ch[1]):
+                names.append(ch[0])
+        return names
+
+
+class Player:
+    def __init__(self, name, tableau):
+        self.name = name
+        self.placed = []
+        self.lost = []
+        self.numbers = Counter()
+        self.tableau = tableau.copy()
+
+    def get_color(self):
+        colors = ('red', 'green', 'yellow', 'cyan')
+        color = self.name.lower() if self.name.lower() in colors else 'blue'
+        return color
+
+    def render_changes(self):
+        '''Renders changes to player that happened within current round.'''
+        content = ''
+        counter = player.numbers
+
+        explored = ''
+        if  counter['explored']:
+            explored = '+%s(%s)' % (counter['kept'], counter['explored'])
+
+        lost = ', '.join(self.lost)
+        placed = ', '.join(self.placed)
+
+        cards = counter['cards']
+        cards = '' if not cards else '+%scards' % cards
+        points = counter['points']
+        points = '' if not points else '+%spoints' % points
+
+        content += ' '.join([explored, cards, points]).strip()
+
+        if not (lost or placed or content):
+            return
+
+        with tag('ul'):
+            if lost:
+                line('li', lost, klass='strike')
+            if placed:
+                line('li', placed)
+            if content:
+                line('li', content)
+
+        # TODO: kinds of goods sometimes have gaps between
+        # them for no apparent reason.
+        for kind in ('novelty', 'rare', 'gene', 'alien'):
+            # Don't add empty spans to DOM tree
+            if counter[kind]:
+                with tag('span', klass=kind):
+                    text('#' * counter[kind])
 
 
 class Phase:
@@ -83,20 +163,9 @@ class Phase:
         self.name = re.search(r'--- (?:Second )?(\w+) phase ---', msg).group(1)
         # might be lower case - "Second settle phase" in 2 player advanced:
         self.name = self.name.title()
-        self.bonuses = []
         self.players = []
         for player_name in tableau:
-            player = {
-                    'name': player_name,
-                    'placed': [],
-                    'lost': [],
-                    'numbers': Counter(),
-                    'tableau': tableau[player_name].copy(),
-                    }
-            self.players.append(player)
-        for player_name, choice in choices:
-            if get_phase_name(choice) == self.name:
-                self.bonuses.append((player_name, choice))
+            self.players.append(Player(player_name, tableau[player_name]))
 
     def update(self, msg, fmt, tableau):
         ''' Update phase based on log message '''
@@ -104,9 +173,9 @@ class Phase:
         # Not checking 'split()[0] in line' because name might be
         # multi-word.
         for pl in self.players:
-            if msg.startswith(pl['name']):
+            if msg.startswith(pl.name):
                 player = pl
-                counter = player['numbers']
+                counter = player.numbers
                 break
         if not player:
             return
@@ -123,8 +192,8 @@ class Phase:
             pattern = r'.+ places ([^.]+) at zero cost|.+ places ([^.]+)'
             match = re.search(pattern, msg)
             placed = match.group(1) or match.group(2)
-            player['placed'].append(placed)
-            tableau[player['name']].append(placed)
+            player.placed.append(placed)
+            tableau[player.name].append(placed)
 
         # Cards discarded FROM TABLEAU (not from hand) can be distinguished
         # by *lack* of format in the message.
@@ -137,8 +206,8 @@ class Phase:
                 pass
             else:
                 lost = re.search(r'.+ discards ([^.]+).', msg).group(1)
-                player['lost'].append(lost)
-                tableau[player['name']].remove(lost)
+                player.lost.append(lost)
+                tableau[player.name].remove(lost)
 
 
         # cards and VPs gained:
@@ -170,59 +239,6 @@ class Phase:
             counter[PRODUCERS[planet]] += 1
 
 
-def render_header(rnd):
-    line('td', 'Actions')
-    for player in rnd[0].players:
-        tab = '#' * len(player['tableau'])
-        # Split tableau into groups of 4 because humans can't naturally
-        # perceive amounts higher than 4.
-        tab = [tab[n:n+4] for n in range(0, len(tab), 4)]
-        tab = ' '.join(tab)
-        with tag('td', klass=get_color(player['name'])):
-            with tag('ul'):
-                line('li', player['name'])
-                line('li', tab)
-
-
-def render_gains(player):
-    content = ''
-    pl = player
-    counter = player['numbers']
-
-    explored = ''
-    if  counter['explored']:
-        explored = '+%s(%s)' % (counter['kept'], counter['explored'])
-
-    lost = ', '.join(pl['lost'])
-    placed = ', '.join(pl['placed'])
-
-    cards = counter['cards']
-    cards = '' if not cards else '+%scards' % cards
-    points = counter['points']
-    points = '' if not points else '+%spoints' % points
-
-    content += ' '.join([explored, cards, points]).strip()
-
-    if not (lost or placed or content):
-        return
-
-    with tag('ul'):
-        if lost:
-            line('li', lost, klass='strike')
-        if placed:
-            line('li', placed)
-        if content:
-            line('li', content)
-
-    # TODO: kinds of goods sometimes have gaps between
-    # them for no apparent reason.
-    for kind in ('novelty', 'rare', 'gene', 'alien'):
-        # Don't add empty spans to DOM tree
-        if counter[kind]:
-            with tag('span', klass=kind):
-                text('#' * counter[kind])
-
-
 tableau = OrderedDict()
 msg = messages[0][0]
 while 'Round' not in msg:
@@ -233,20 +249,21 @@ while 'Round' not in msg:
 
 
 rounds = []
+round_nr = 1
 while 'End of game' not in msg:
-    choices = []
-    rnd = []
+    rnd = Round(round_nr)
     while 'phase ---' not in msg:
         # Between Round and Phase
-        choices = updated_choices(choices, msg)
+        rnd.update_choices(msg)
         msg, fmt = messages.pop(0)
     while '===' not in msg:
         if 'phase ---' in msg:
-            rnd.append(Phase(msg, choices, tableau))
+            rnd.phases.append(Phase(msg, rnd.choices, tableau))
         else:
-            rnd[-1].update(msg, fmt, tableau)
+            rnd.phases[-1].update(msg, fmt, tableau)
         msg, fmt = messages.pop(0)
     rounds.append(rnd)
+    round_nr += 1
 
 
 doc, tag, text, line= Doc().ttl()
@@ -254,20 +271,20 @@ with tag('html'):
     with tag('meta'):
         doc.stag('link', rel="stylesheet", href="style.css")
     with tag('body'):
-        for round_number, rnd in enumerate(rounds, 1):
-            line('h2', 'Round %s' % round_number)
+        for rnd in rounds:
+            line('h2', 'Round %s' % rnd.number)
             with tag('table'):
                 with tag('tr'):
-                    render_header(rnd)
-                for phase in rnd:
+                    rnd.render_header()
+                for phase in rnd.phases:
                     with tag('tr'):
                         line('td', ROMAN[phase.name])
                         for player in phase.players:
                             klass = ''
-                            if player['name'] in (b[0] for b in phase.bonuses):
-                                klass = get_color(player['name'])
+                            if player.name in rnd.phase_played_by(phase):
+                                klass = player.get_color()
                             with tag('td', klass=klass):
-                                render_gains(player)
+                                player.render_changes()
 
 
 output = open('report.html', 'w')
